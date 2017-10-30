143dde9277a6b5c66522a46bce684602b2c8baaa
trong-nguyen <nguyentrong@gmail.com>
2017-10-30 19:19:34 -0400

    First AWS Lambda function created and operational. Deployment carried out through AWS CLI

---
+++ aws_lambda/twitter.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def handler(event, context):
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    print("Received event: " + json.dumps(event, indent=2))
+
+    return respond(None, {
+        'message' : 'Hello Lambda!',
+        # 'event'   : json.dumps(event),
+        # 'context' : json.dumps(context)
+        })
---
+++ aws_lambda/update_lambda.sh
@@ -0,0 +1,5 @@
+zip -r ./app.zip twitter.py requests &
+aws lambda update-function-code \
+--function-name   twit \
+--zip-file fileb:///Users/nguyentrong/Dropbox/Projects/udacity-fullstack/aws_lambda/app.zip \
+--publish
